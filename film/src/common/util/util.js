fd48d380efae616def5a055e0312dc7add664a42
J1ong <2419782227@qq.com>
2019-08-22 19:45:50 +0800

    fixed：修复用户评论日期不正常显示问题

--- film/src/common/util/util.js
+++ film/src/common/util/util.js
@@ -25,7 +25,7 @@ export const formatDate = function(date,flag){
                     }
                 }
             }else{
-               return (date.getMonth()+1)>9?(date.getMonth()+1):'0'+(date.getMonth()+1)+'-'+date.getDate()>9?date.getDate():'0'+date.getDate()
+               return (date.getMonth()+1>9?(date.getMonth()+1):'0'+(date.getMonth()+1))+'-'+(date.getDate()>9?date.getDate():'0'+date.getDate())
             }
         }
     }else{
